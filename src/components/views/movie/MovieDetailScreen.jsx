65b47ab54503170ca8265c91277947dae0f7cc34
Jelle Geraets <jellegeraets@hotmail.com>
2020-01-08 22:32:05 +0100

    added rating and comments to the view

--- src/components/views/movie/MovieDetailScreen.jsx
+++ src/components/views/movie/MovieDetailScreen.jsx
@@ -3,6 +3,10 @@ import {withRouter} from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 import MovieCalls from "../../apicalls/MovieCalls";
 import Button from "@material-ui/core/Button";
+import {Card} from "@material-ui/core";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
 
 const styles = theme => ({ //todo move to other file and import
 });
@@ -24,7 +28,8 @@ class MovieDetailScreen extends Component {
     movieCalls = new MovieCalls();
 
     state = {
-        movie: {id: -1, title: "", description: "", link: "", genres: []},
+        movie: {id: -1, title: "", description: "", link: "", genres: [], comments:[], ratings:[]},
+        rating: {totalRating: 0, ratingCount: 0}
     };
 
     movieReturned() {
@@ -33,6 +38,14 @@ class MovieDetailScreen extends Component {
             this.props.history.push({
                 pathname: "/movies"
             });
+        } else {
+            let totalRating = 0.0;
+            let count = 0;
+            this.state.movie.ratings.map(rating => {
+                totalRating += rating.value;
+                count++;
+                });
+            this.setState({rating: {totalRating: totalRating / count, ratingCount: count}});
         }
     }
 
@@ -67,11 +80,29 @@ class MovieDetailScreen extends Component {
                 Genres:
                 <br/>
                 <ul>
-                {this.state.movie.genres.map(genre => {
-                    return <li key={genre.id} onClick={() => this.toGenreDetails(genre.id)}>{genre.name}</li>
-                })}
+                    {this.state.movie.genres.map(genre => {
+                        return <li key={genre.id} onClick={() => this.toGenreDetails(genre.id)}>{genre.name}</li>
+                    })}
                 </ul>
-
+                <br/><br/>
+                Rating: {this.state.rating.totalRating}/10 (by {this.state.rating.ratingCount} users)]
+                <br/><br/>
+                Comments:
+                <br/>
+                    {this.state.movie.comments.map(comment => {
+                        return (
+                            <Card className={classes.card}>
+                                <CardHeader
+                                    title={"comment.user.email"}
+                                    subheader={"comment.date"}
+                                />
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {"comment.text"}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                            )})}
                 <Button variant="contained" className={classes.button}
                     onClick={() => this.editMovie()}>
                     Edit
